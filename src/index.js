5ced91b7c9931239df0c8fbdc7d8314c4ae6f0b2
NyakundiMauya <n.mauya.nm@gmail.com>
2023-04-03 10:30:38 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+  const taskList = document.querySelector("#tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault(); // preventing the default behavior 
+    const newTask = document.createElement("li");
+    newTask.innerText = document.querySelector("#new-task-description").value;
+    taskList.appendChild(newTask);
+    addDeleteButton(newTask); 
+    event.target.reset(); // clears input
+  });
+
+  function addDeleteButton(task) {
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "x";
+    task.appendChild(deleteButton);
+    deleteButton.addEventListener("click", () => {
+      task.remove(); // delete from task list
+    });
+  }
 });
